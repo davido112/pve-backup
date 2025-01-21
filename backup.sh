1afe10ab756a9e416a06fed8e261a8a21213532a
Dave <97832647+davido112@users.noreply.github.com>
2025-01-21 09:11:36 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,16 +1,15 @@
-#! /bin/bash
 
-next_id=`pvesh get /cluster/nextid`
-max=${next_id:1:2} # ${sting:index:length}
+#!/bin/bash
+
+
+# Lekéri a létrehozott virtuális gépeket és formázza a szöveget használható formátumba
+ids=( `pvesh get /cluster/resources --type vm | awk -F' |/' '{print $3}' | grep -v '^$'` )
+idscount=${#ids[@]}
 date=`date +"%F"`
 
-
-for (( i=0;i<$max;i++  ))
+# Csinál az összes virtuális eszközről egy mentést a /backup mappába
+for ((i=0;i<idscount;i++))
  do
-  if (( $max <= 9  ))
-   then
-    write='0'$i
-   fi
-  #vzdump 10$i --mode snapshot --remove 0 --storage local --compress zstd --notification-mode auto --notes-template '1'$write'_backup-'$date --node pve1tszt
-  echo 'A backup sikeresen lefutott: 1'$write'_backup-'$date' virtuális eszközön.'
+  vzdump ${ids[i]} --mode snapshot --dunpdir /backup/  --remove 0 --storage local --compress zstd --notification-mode auto --notes-template ${ids[i]}_backup-$date --node pve1tszt
+  echo 'A backup sikeresen lefutott: '${ids[i]}'_backup-'$date' virtuális eszközön.'
 done
