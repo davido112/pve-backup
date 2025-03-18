c146d2796a02afe48915480f24c908884bf0fb08
Dave <97832647+davido112@users.noreply.github.com>
2025-03-18 16:29:06 +0100

    Finished

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-# Scan the created virtual machines and format the text into a usable format
+# Lekéri a létrehozott virtuális gépeket és formázza a szöveget használható formátumba
 ids=( `pvesh get /cluster/resources --type vm | awk -F' |/' '{print $3}' | grep -v '^$'` )
 idscount=${#ids[@]}
 date=`date +"%Y%m%d"`
@@ -9,9 +9,13 @@ backupdir="/backup" # Write me
 new_backupdir=$backupdir/$date
 config_dir="/etc/pve/nodes/"`hostname`"/qemu-server"
 
+# Remove old backups
+find "$backupdir" -mtime +7 -exec rm -rf '{}' \;
+
+# Make daystamped folder
 mkdir -p $new_backupdir
 
-# Make a backup of all virtual devices to the /backup/yearmonthday folder
+# Csinál az összes virtuális eszközről egy mentést a /backup mappába
 for ((i=0;i<idscount;i++))
  do
   cp $config_dir/${ids[i]}".conf" $new_backupdir/
@@ -19,4 +23,5 @@ for ((i=0;i<idscount;i++))
   name=`cat $new_backupdir/${ids[i]}".conf" | grep "name: " | sed -e "s/name: //"`
   echo 'A backup sikeresen lefutott: '${ids[i]}'_backup-'$name'-'$date' virtuális eszközön.'
   find "$new_backupdir" -type f -name "*qemu-${ids[i]}*.vma.zst" -exec mv {} $new_backupdir/${ids[i]}'_backup-'$name'-'$date".vma.zst" \;
+  find "$new_backupdir" -type f -name "*qemu-${ids[i]}*.log" -exec mv {} $new_backupdir/${ids[i]}'_backup-'$name'-'$date".log" \;
 done
