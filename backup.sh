b6ef55ac44bc9945d9d07ec4cca3c1721faaf679
Dave <97832647+davido112@users.noreply.github.com>
2025-03-18 09:03:41 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,10 +4,12 @@
 # Lekéri a létrehozott virtuális gépeket és formázza a szöveget használható formátumba
 ids=( `pvesh get /cluster/resources --type vm | awk -F' |/' '{print $3}' | grep -v '^$'` )
 idscount=${#ids[@]}
-date=`date +"%Y%M%D"`
+date=`date +"%Y%m%d"`
 backupdir="/backup" # Write me
 new_backupdir=$backupdir/$date
 config_dir="/etc/pve/nodes/"`hostname`"/qemu-server"
+
+mkdir -p $new_backupdir
 
 # Csinál az összes virtuális eszközről egy mentést a /backup mappába
 for ((i=0;i<idscount;i++))
@@ -15,7 +17,7 @@ for ((i=0;i<idscount;i++))
   cp $config_dir/${ids[i]}".conf" $new_backupdir/
   vzdump ${ids[i]} --mode snapshot --dumpdir $new_backupdir --compress zstd --node pve1tszt
   echo 'A backup sikeresen lefutott: '${ids[i]}'_backup-'$date' virtuális eszközön.'
-  find "qemu-${volume[idscount]}"
+  find $new_backupdir "qemu-${volume[idscount]}"
 done
 
 # 100_NÉV_backup_2025_01_01
